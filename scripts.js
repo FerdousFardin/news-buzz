bf527953e9d15db889436917a0ac8f9bab15d0cb
FerdousFardin <fardinh39u@gmaile.com>
2022-09-02 22:54:45 +0600

    catergories added

--- scripts.js
+++ scripts.js
@@ -8,13 +8,44 @@ const loadCategory = async () => {
 const displayCategory = (categories) => {
   //   console.log(categories);
   const categoryContainer = document.getElementById("category-nav");
-  categoryContainer.innerHTML = "";
   categories.forEach((category) => {
     const { category_id: categoryId, category_name: categoryName } = category;
+    const elementId = categoryName.split(" ").join("-");
+    // console.log(elementId);
     const categoryAnchor = document.createElement("a");
     categoryAnchor.classList.add("tab");
+    // categoryAnchor.classList.toggle("tab-active");
+    categoryAnchor.setAttribute(
+      "onclick",
+      `loadNews('${categoryId}','${elementId}')`
+    );
+    categoryAnchor.setAttribute("id", `${elementId}`);
     categoryAnchor.innerText = categoryName;
     categoryContainer.append(categoryAnchor);
   });
+};
+const loadNews = async (categoryId, elementId) => {
+  const res = await fetch(
+    `https://openapi.programming-hero.com/api/news/category/${categoryId}`
+  );
+  const data = await res.json();
+  displayNews(data.data, elementId);
+};
+const tabActiveToggler = (shouldHave) => {
+  const tabs = document.querySelectorAll(".tab");
+  tabs.forEach((tab) => {
+    tab.classList.remove("tab-active-custom");
+  });
+  const activeTab = document.querySelector(`#${shouldHave}`);
+  activeTab.classList.add("tab-active-custom");
+};
+const displayNews = (articles, elementId) => {
+  //active class toggler
+  tabActiveToggler(elementId);
+  //spinner
+  //display articles
+  const articlesContainer = document.getElementById("articles-container");
+  articlesContainer.innerHTML = "";
+  articles.forEach((article) => {});
 };
 loadCategory();
